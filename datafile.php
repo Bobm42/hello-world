1f20323ca1421df7ba8a9b475f77528bbb22bf8c
Bobm42 <Bobm42@users.noreply.github.com>
2018-04-27 10:58:56 +0100

    Add results to datafile

--- datafile.php
+++ datafile.php
@@ -49,6 +49,7 @@ function ramdon() {
 </head>
 <body>
 
+include 'config.php';
 
 <h1>Search</h1>
 
@@ -77,6 +78,70 @@ if ($_SERVER ['REQUEST_METHOD'] == "POST") {
 <p id="demod" >+<p id="demo">+<p id="demom">+<p id="demoy"></p>
 
 <p id="dem"></p>
+
+if ($result = mysqli_query($conn, "SELECT * FROM dogs order by name")) {
+	printf("<p> Select returned %d rows.\n </p>", mysqli_num_rows($result));
+}
+
+	echo "<table border='1'>";
+	echo "<tr>";
+	echo "<th>". "NAME" ."</th>";
+	echo "<th>". "COLOUR" ."</th>";
+	echo "<th>". "BREED" ."</th></tr>";
+while($row = mysqli_fetch_array($result)){
+	echo "<tr><td>" . $row['name'] ."</td>";	
+	echo "<td>" . " ". $row['colour'] ."</td>";
+	echo "<td>" . " ". $row['breed'] ."</td></tr>";
+	}
+		echo "</table>";
+
+mysqli_free_result($result);
+
+
+if ($result = mysqli_query($conn, "SELECT * FROM cars order by name")) {
+	printf("<p> Select returned %d rows.\n </p>", mysqli_num_rows($result));
+}
+
+	echo "<table border='1'>";
+	echo "<tr>";
+	echo "<th>". "NAME" ."</th>";
+	echo "<th>". "MAKE" ."</th>";
+	echo "<th>". "COLOUR" ."</th>";
+	echo "<th>". "COUNTRY" ."</th></tr>";
+
+while($row = mysqli_fetch_array($result)){
+
+	echo "<tr><td>" . $row['name'] ."</td>";	
+	echo "<td>" . $row['make'] ."</td>";
+	echo "<td>" . $row['colour'] ."</td>";
+	echo "<td>" . $row['country'] ."</td></tr>";
+
+	}
+		echo "</table>";
+	
+mysqli_free_result($result);
+
+
+if ($result = mysqli_query($conn, "SELECT * FROM fruit order by name")) {
+	printf("<p> Select returned %d rows.\n </p>", mysqli_num_rows($result));
+}
+	echo "<table border='1'>";
+	echo "<tr>";
+	echo "<th>". "NAME" ."</th>";
+	echo "<th>". "TYPE" ."</th>";
+	echo "<th>". "COLOUR" ."</th>";
+	echo "<th>". "JOIN" ."</th>";
+	echo "<th>". "COUNTRY" ."</th></tr>";
+while($row = mysqli_fetch_array($result)){
+	echo "<tr><td>" . $row['name'] ."</td>";	
+	echo "<td>" . $row['type'] ."</td>";
+	echo "<td>" . $row['colour'] ."</td>";
+	echo "<td>" . $row['colour'] . "mailto:". $row['type'] ."</td>";
+	echo "<td>" . $row['country'] ."</td></tr>";
+	}
+		echo "</table>";
+		
+mysqli_free_result($result);
 
 </body>
 </html>
